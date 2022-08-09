340e5704486f94ec2ad542c4ad66248dae6e42d7
김영목 <mokyoungg@gmail.com>
2022-08-09 18:11:03 +0900

    feat: error handeling in global - install react-toastify - use SWRConfig

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GlobalStyle } from './styles/globalStyle';
 import App from './App';
+import ToastContainer from './components/ToastContainer/ToastContainer';
+import { triggerErrorToast } from './utils/triggerToast';
+import 'react-toastify/dist/ReactToastify.css';
+import { SWRConfig } from 'swr';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <SWRConfig
+      value={{
+        onError: (error) => {
+          const { message } = error;
+          triggerErrorToast(message);
+        },
+      }}
+    >
+      <GlobalStyle />
+      <App />
+      <ToastContainer
+        position="bottom-left"
+        theme="colored"
+        autoClose={3000}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+      />
+    </SWRConfig>
   </React.StrictMode>,
 );
